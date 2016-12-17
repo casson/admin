5cc87a1d2e553a83ab2f73042f0a346fd436c509
zhouzhe1157@qq.com <zhouzhe1157@qq.com>
2016-12-17 14:31:12 +0800

    修改本地git账户

--- backend/module/admin/controllers/DefaultController.php
+++ backend/module/admin/controllers/DefaultController.php
@@ -165,7 +165,7 @@ class DefaultController extends EController
         echo "</script>";
     }
     
-    //锁屏功能
+    //锁屏功能  
     public function actionLockScreen()
     {
         Yii::app()->session['admin_id']='';
